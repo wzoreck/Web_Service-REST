60b252791f1cea03ea99105d58ab6759e238ef49
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-12-03 09:31:19 -0300

    Get data from API REST and show on template

--- consumer/weather/views.py
+++ consumer/weather/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import View
+import requests
 
-# Create your views here.
+class Index(View):
+    template_name = 'weather/index.html'
+
+    def setup(self, *args, **kwargs):
+        super().setup(*args, **kwargs)
+        
+        data_request = requests.get(url='http://127.0.0.1:8080/data/') # URL GET API
+        print(data_request.json())
+
+        self.context = {
+            'data': data_request.json(),
+        }
+
+        self.renderizar = render(self.request, self.template_name, self.context)
+
+    def get(self, *args, **kwargs):
+        return self.renderizar
