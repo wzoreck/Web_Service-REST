9a1eb50b3df9b8175e7af82dabee41f2ecded9f9
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-12-03 11:40:46 -0300

    Show data in Edit page and Delete added on index

--- consumer/weather/urls.py
+++ consumer/weather/urls.py
@@ -5,5 +5,6 @@ app_name = 'weather'
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
-    path('edit/', views.Edit.as_view(), name='edit'),
+    path('edit/<int:pk>', views.Edit.as_view(), name='edit'),
+    path('delete/<int:pk>', views.Delete.as_view(), name='delete'),
 ]
--- consumer/weather/views.py
+++ consumer/weather/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 import requests
 
@@ -8,8 +8,8 @@ class Index(View):
     def setup(self, *args, **kwargs):
         super().setup(*args, **kwargs)
         
+        #GET ALL API
         data_request = requests.get(url='http://127.0.0.1:8080/data/') # URL GET API
-        print(data_request.json())
 
         self.context = {
             'data': data_request.json(),
@@ -20,16 +20,36 @@ class Index(View):
     def get(self, *args, **kwargs):
         return self.renderizar
 
+
 class Edit(View):
     template_name = 'weather/edit.html'
 
     def setup(self, *args, **kwargs):
         super().setup(*args, **kwargs)
         
+        pk = self.kwargs.get('pk')
+        
+        # GET API
+        data = requests.get(url=f'http://127.0.0.1:8080/data/{pk}/')
+        
         self.context = {
+            'data': data.json(),
         }
 
         self.renderizar = render(self.request, self.template_name, self.context)
 
     def get(self, *args, **kwargs):
         return self.renderizar
+
+
+class Delete(View):
+    def setup(self, *args, **kwargs):
+        super().setup(*args, **kwargs)
+        
+        pk = self.kwargs.get('pk')
+        
+        # GET API
+        requests.delete(url=f'http://127.0.0.1:8080/data/{pk}/')
+
+    def get(self, *args, **kwargs):
+        return redirect('weather:index')
