1aab04b2d594b69af38e972e9be807b2c2eeb033
Daniel Wzoreck <wzoreck2000@gmail.com>
2021-12-03 14:03:43 -0300

    Send PUT to API

--- consumer/weather/views.py
+++ consumer/weather/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
 import requests
+import json
 
 class Index(View):
     template_name = 'weather/index.html'
@@ -27,10 +28,10 @@ class Edit(View):
     def setup(self, *args, **kwargs):
         super().setup(*args, **kwargs)
         
-        pk = self.kwargs.get('pk')
+        self.pk = self.kwargs.get('pk')
         
         # GET API
-        data = requests.get(url=f'http://127.0.0.1:8080/data/{pk}/')
+        data = requests.get(url=f'http://127.0.0.1:8080/data/{self.pk}/')
         
         self.context = {
             'data': data.json(),
@@ -41,6 +42,25 @@ class Edit(View):
     def get(self, *args, **kwargs):
         return self.renderizar
 
+    def post(self, *args, **kwargs):
+        temperature = self.request.POST.get('temperature').replace(',', '.')
+        humidity = self.request.POST.get('humidity').replace(',', '.')
+        luminosity = self.request.POST.get('luminosity').replace(',', '.')
+        date = self.request.POST.get('date')
+        time = self.request.POST.get('time')
+        
+        load = {
+            'temperature': float(temperature),
+            'humidity': float(humidity),
+            'luminosity': float(luminosity),
+            'date': str(date),
+            'time': str(time),
+        }
+        
+        # PUT API
+        r = requests.put(url=f'http://127.0.0.1:8080/data/{self.pk}/', json=load)
+        
+        return redirect('weather:index')
 
 class Delete(View):
     def setup(self, *args, **kwargs):
